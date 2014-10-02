37d69dd72c27a7e983e38d9dddab609b092d0328
AlbertoEAF <albertoeafworks@gmail.com>
2014-10-02 16:24:13 +0100

    Add bss_eval_ibm.m

---
+++ bss_eval_ibm.m
@@ -0,0 +1,19 @@
+function [SDR,SIR,SAR,perm SDRe,SIRe,SARe,perme] = bss_eval_ibm (logfile)
+
+Spattern  = 'sounds/s*x0.wav';
+Sepattern = 'ibmx*_rebuilt.wav';
+
+[SDR,SIR,SAR,perm, SDRe,SIRe,SARe,perme] = bss_eval(Sepattern, Spattern);
+
+diary on
+diary(logfile);
+
+% Print the best estimated source match for each true source.
+fprintf('e_'); print_bss_eval_stats(SDR , SIR , SAR , perm );
+% Print the best original match for each estimated source.
+fprintf('o_'); print_bss_eval_stats(SDRe, SIRe, SARe, perme);
+
+
+
+end
+
